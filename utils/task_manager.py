90cc54a312db771b4f741cf4be61a603f4babafd
kesha1225 <samedov03@mail.ru>
2019-12-26 19:17:04 +0300

    added tak_manager

---
+++ utils/task_manager.py
@@ -0,0 +1,27 @@
+import asyncio
+import typing
+
+
+class TaskManager:
+    def __init__(self, loop: asyncio.AbstractEventLoop):
+        self.tasks: typing.List[typing.Callable] = []
+        self.loop = loop
+
+    def add_task(self, task: typing.Callable):
+        if asyncio.iscoroutinefunction(task):
+            self.tasks.append(task)
+        else:
+            raise TypeError("Таск может быть только корутиной")
+
+    async def __run(self):
+        if not self.tasks:
+            raise RuntimeError("Таск менеджер получил 0 заданий")
+
+        [await self.loop.create_task(task()) for task in self.tasks]
+
+    def run(self, on_shutdown: typing.Callable = None):
+        try:
+            self.loop.run_until_complete(self.__run())
+        finally:
+            if on_shutdown is not None:
+                self.loop.run_until_complete(on_shutdown())
