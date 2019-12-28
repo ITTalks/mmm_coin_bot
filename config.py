594cb15d4d97745814aa52fe5d22ec81bfa68846
kiriharu <kiriharu@github.com>
2019-12-28 10:50:35 +0300

    config update, more comments

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
-invest_to = ["group", 173250756]
+invest_to = ["group", 173250756]  # куда инвестим. Первое - group или юзер, второе - ID
 
 # SIMPLE TACTIC
-mine_count = 500
-sleep_after_mine = 20
-push_limit = 10000
-accs_file_name = "accs.txt"
+mine_count = 400  # количество монеток, которые майним за раз
+sleep_after_mine = 10  # спим после майнинга
+push_limit = 10000000  # После достижения этого лимита, инвестируем монетки
+accs_file_name = "accs.txt"  # файл с аккаунтами
